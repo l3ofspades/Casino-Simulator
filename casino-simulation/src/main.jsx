66aa09b0ca60aa06039533ff429a24779a52008a
I3ofspade <jonathanfederico13@gmail.com>
2025-12-25 10:35:33 -0700

    Fix Render build by restoring App entrypoint

--- casino-simulation/src/main.jsx
+++ casino-simulation/src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </ChipProvider> 
+      </ChipProvider>
     </AuthProvider>
   </StrictMode>
 );
